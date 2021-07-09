9a092a621f38448c23250370ae7ca754c19fc1f5
tlow22 <thaddeuslow@outlook.com>
2021-07-09 16:10:04 -0600

    add fibonacci exercise

--- chapter-3-exercises/fibonnaci/src/main.rs
+++ chapter-3-exercises/fibonnaci/src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
+// generates the n-th fibonnaci number 
 fn main() {
-    println!("Hello, world!");
+    // get user input 
+    println!("Enter n-th Fibonnaci number desired"); 
+
+    let mut idx = String::new();
+    io::stdin()
+        .read_line(&mut idx)
+        .expect("Failed to read");
+
+    let idx:u32 = match idx.trim().parse() {
+        Ok(num) => num, 
+        Err(_)  => 0
+    };
+
+    // generate specified Fibonnaci number 
+    match idx {
+        0 => println!("Invalid input detected. No calculation will be done"),
+        _ => println!("The {}-th Fibonnaci number is {}", idx, fibonnaci(idx)),
+    }
+}
+
+fn fibonnaci(n: u32) -> u32 {
+    if n  <= 1 {
+        return n; 
+    } 
+
+    return fibonnaci(n-1) + fibonnaci(n-2);
 }
