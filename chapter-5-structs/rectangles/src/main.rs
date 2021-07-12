11af5fb5e7806fb866bd9b127a5d5f044049709b
tlow22 <thaddeuslow@outlook.com>
2021-07-12 17:52:33 -0600

    add fit checker

--- chapter-5-structs/rectangles/src/main.rs
+++ chapter-5-structs/rectangles/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32{
         return self.width * self.height
     }
+
+    fn can_fit(&self, other: &Rectangle) -> bool {
+        return self.width >= other.width && self.height >= other.height
+    }
 }
 
 fn main() {
@@ -16,7 +20,28 @@ fn main() {
         height: 20,
     };
 
+    let rect2 = Rectangle {
+        width: 5,
+        height: 10,
+    };
+
+    let rect3 = Rectangle {
+        width: 30,
+        height: 10,
+    };
+
+    let rect4 = Rectangle {
+        width: 20,
+        height: 10,
+    };
+    
+    // print out rectangle info
     println!("rect is {:?}", rect);
     println!("rect area is {}", rect.area());
+    
+    // print if rect can fit rect2, rect3, rect4
+    println!("rect can fit rect2? {}", rect.can_fit(&rect2));
+    println!("rect can fit rect3? {}", rect.can_fit(&rect3));
+    println!("rect can fit rect4? {}", rect.can_fit(&rect4));
 }
 
